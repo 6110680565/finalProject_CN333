3a20193fc9ec75b768279679e577c4be6fd7477c
tham <thamthearawiboon@gmail.com>
2021-05-03 00:35:28 +0700

    Initial Commit

---
+++ MyShop2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyShop2
+//
+//  Created by Tham Thearawiboon on 3/5/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
